bb44cb9beed02a75f8b2fa9187cd2e54c5a05259
Kamalesh <kamalesh5112000@pec.edu.com>
2023-12-04 02:39:36 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
+
+job_title=[
+    'first job',
+    'second job'
+]
+job_description=[
+    'frist job description',
+    'second job description'
+]
 # Create your views here.
+def hello(request):
+    return HttpResponse("Hello World")
+
+def job_detail(request,id):
+    if id==0:
+        return redirect('/')
+    return_html=f"<h1>{job_title[id]}</h1><br><br><h3>{job_description[id]}</h3>"
+    return HttpResponse(return_html)
